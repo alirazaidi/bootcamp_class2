b4b82be043b90098d1ee747a9269b0f19c75bfbc
alirazaidi <alirazaidi@hotmail.com>
2020-11-19 23:07:17 +0500

    Update App.js
    modified my name

--- src/App.js
+++ src/App.js
@@ -9,7 +9,7 @@ function App() {
         <p>
           Edit <code>src/App.js</code> and save to reload.
         </p>
-        <p>Hellow World from ali raza</p>
+        <p>Hellow World from ali raza Zidi</p>
         <a
           className="App-link"
           href="https://reactjs.org"
